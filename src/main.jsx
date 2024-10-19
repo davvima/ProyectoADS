d43fae75bbc1d16e8d668ede8f4bd6b36647c6ea
davvima <dfreites7@gmail.com>
2024-10-18 22:52:24 -0300

    origin

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    authorizationParams={{ redirect_uri: "http://localhost:5173/admin" }}
+    authorizationParams={{ redirect_uri: window.location.origin }}
   >
     <ThemeProvider theme={theme}>
       <BrowserRouter>
