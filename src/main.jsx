3967d880551fbe6e5553e9b1be362fc611686aba
davvima <dfreites7@gmail.com>
2024-10-18 23:20:30 -0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom"
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
-
+console.log({ domain, clientId })
 createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain={domain}
